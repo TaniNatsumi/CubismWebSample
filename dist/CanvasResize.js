4d554e1df8bba3620395957b9ad2c3a5ac53f1c0
TaniNatsumi <58015504+TaniNatsumi@users.noreply.github.com>
2019-12-15 06:16:31 +0900

    Add files via upload

--- dist/CanvasResize.js
+++ dist/CanvasResize.js
@@ -1,14 +1,18 @@
-(function() {
-    var canvas = document.getElementById('SAMPLE');
+  // リサイズ処理
+function glResize(){
+    var glCanvas1 = document.getElementById('SAMPLE');
     var container = document.getElementById('wrap');
-    sizing();
-  
-    function sizing() {
-      canvas.height = container.offsetHeight;
-      canvas.width = container.offsetWidth;
-    }
-  
-    window.addEventListener('resize', function() {
-      (!window.requestAnimationFrame) ? setTimeout(sizing, 300): window.requestAnimationFrame(sizing);
-    });
-  })();
+
+    container.style.width = "60%";
+    container.style.margin = "0 auto 0 auto";
+
+    // 元の画像サイズを保持
+    var orgWidth  = glCanvas1.canvas.width;
+    var orgHeight = glCanvas1.canvas.height;
+    // 変更する画像サイズを指定
+    glCanvas1.canvas.width = container.clientWidth;
+    glCanvas1.canvas.height = orgHeight * (glCanvas1.canvas.width / orgWidth);
+    // glでの表示サイズもCanvasに合わせる
+    gl.viewport(0,0,glCanvas1.canvas.width, glCanvas1.canvas.height);
+}
+glResize();
